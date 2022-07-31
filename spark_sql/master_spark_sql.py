9575c079f9f39b2af4b2950744db5fe684ba0b5e
Z Perry Liu <68303975+perryisbusy@users.noreply.github.com>
2022-07-31 03:06:28 -0400

    Add files via upload

---
+++ spark_sql/master_spark_sql.py
@@ -0,0 +1,25 @@
+# Databricks notebook source
+dbutils.notebook.run("/playground/spark_sql/Spark SQL Local & Global View 0", 0);
+
+# COMMAND ----------
+
+dbutils.notebook.run(path="/playground/spark_sql/Spark SQL DB & Table & View 1", timeout_seconds=0);
+
+# COMMAND ----------
+
+dbutils.notebook.run("/playground/spark_sql/Spark SQL Raw & Processed & Presentation 2", 0);
+
+# COMMAND ----------
+
+dbutils.notebook.run("/playground/spark_sql/Spark SQL DDL & DML", 0);
+
+# COMMAND ----------
+
+dbutils.notebook.run("/playground/spark_sql/Spark SQL Analysis", 0);
+
+# COMMAND ----------
+
+dbutils.notebook.exit("master_spark_sql")
+
+# COMMAND ----------
+
